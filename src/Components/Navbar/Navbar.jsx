67e083f4bf77863c5bbeb0bd4230f006788817ee
lucky-KRMU <2501730142@krmu.edu.in>
2026-04-25 04:24:26 +0000

    add google font

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-    This is navbar
+    <header className='w-[20%] h-screen bg-violet-950 text-white flex items-center justify-center'>
+        <nav>
+            <ul className='flex flex-col items-center justify-center text-xl'>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+        </nav>
+    </header>
     </>
   )
 }
